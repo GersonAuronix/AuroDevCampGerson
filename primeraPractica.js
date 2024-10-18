d36ddbf7728cb0f4d722592afdc28dbb40009db5
gersonAuronix <ghernandez@auronix.com>
2024-10-18 17:46:11 -0600

    Cuota por km

--- primeraPractica.js
+++ primeraPractica.js
@@ -1,6 +1,4 @@
-//console.log('Hola, esto es una prueba');
 const args = process.argv.slice(2);
-//console.log('Argumentos recibidos:', args);
 
 switch (args[0]) {
   case 'h':
@@ -9,6 +7,9 @@ switch (args[0]) {
     console.log(`importe: ${importe} & ganancia: ${ganancia}`);
     break;
   case 'k':
+    const importekm = 100 + Number(args[1]) * 10;
+    const gananciakm = importekm * .45;
+    console.log(`importe: ${importekm} & ganancia: ${gananciakm}`);
     break;
   default:
     break;
