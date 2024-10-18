611053b492adb972a234eed029744f71187f6579
gersonAuronix <ghernandez@auronix.com>
2024-10-18 17:40:51 -0600

    Cuota por hora

--- primeraPractica.js
+++ primeraPractica.js
@@ -1,10 +1,12 @@
 //console.log('Hola, esto es una prueba');
 const args = process.argv.slice(2);
-console.log('Argumentos recibidos:', args);
+//console.log('Argumentos recibidos:', args);
 
 switch (args[0]) {
   case 'h':
-
+    const importe = Number(args[1]) * 99;
+    const ganancia = importe * .45;
+    console.log(`importe: ${importe} & ganancia: ${ganancia}`);
     break;
   case 'k':
     break;
