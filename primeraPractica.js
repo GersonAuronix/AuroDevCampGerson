739ba5e5832702a6866589aff72675cbb79ba15e
gersonAuronix <ghernandez@auronix.com>
2024-10-18 14:19:00 -0600

    primer commit

---
+++ primeraPractica.js
@@ -0,0 +1,13 @@
+//console.log('Hola, esto es una prueba');
+const args = process.argv.slice(2);
+console.log('Argumentos recibidos:', args);
+
+switch (args[0]) {
+  case 'h':
+
+    break;
+  case 'k':
+    break;
+  default:
+    break;
+}
